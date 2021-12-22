4ea435c3820916fe2e1de4543a0736327cfa15ff
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2021-12-23 01:33:55 +0800

    added coding challenge #2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -242,7 +242,7 @@ console.log(`ES6 uses backticks
 to write multi-line strings
 like in this example
 here.`);
-*/
+
 
 // 18. Taking decisions using if / else statements
 
@@ -265,5 +265,17 @@ if (birthYear <= 2000) {
     century = 21;
     console.log(`You are born in the ${century}st century`);
 }
+*/
 
+// Coding Challenge #2
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+// Your tasks:
+// 1. Print a nice output to the console, saying who has the higher BMI.The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+//
+// 2. Use a template literal to include the BMI values in the outputs.Example: "Mark's
+// BMI(28.3) is higher than John's (23.9)!"
+//
+// Hint: Use an if/else statement 😉
+// GOOD LUCK 😀
 
