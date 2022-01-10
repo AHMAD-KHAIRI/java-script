caebeeca0295bc0537b7fda1426effd202fa387d
KHAIRI AHMAD (DCAZ/SLF-MY) <ahk1shm@bosch.com>
2022-01-11 00:23:24 +0400

    added examples of switch statements and exercise

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -546,10 +546,51 @@ if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >= 100) {
 } else {
     console.log("No team wins the trophy!");
 }
-*/
+
 
 // 26. The switch statement
 
 const day = 'monday';
 
+switch (day) {
+    case 'monday': // day === 'monday' (switch does a strict equality comparison)
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+*/
+// Exercise: translate the above to if else statements
 
+// Hint: use logical operator to translate the case of 'wednesday' and 'thursday' case
+const day = 'monday';
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
