286290439bcdfde5505e416b25d866c2ff9bcacd
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-02-20 12:40:59 +0800

    Added JavaScript functions with parameters

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 /*
 // 20 Feb 2022
-// activate strict mode to make code secure
+// activate strict mode to make code more secure
 'use strict';
 
 // strict mode creates visible errors in the dev console where in other case JS failed silently
@@ -22,9 +22,26 @@ const private = '1234';
 // JavaScript functions
 
 // a function is a piece of code that we can reuse in the code as many times we want
+// function <function-name>(<function-parameter>) {<statements>}
+// let's do a first example
 function logger() {
     console.log('My name is AK!');
 }
 
 // calling / running / invoking function
 logger();
+logger();
+logger();
+
+// second example
+// parameters are variables that are specific to the function
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and 
+    ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
