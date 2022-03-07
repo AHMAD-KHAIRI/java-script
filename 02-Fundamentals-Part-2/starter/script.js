9f9e18f04e5dfa848d5d184f0d05da49fbd725a1
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-03-07 12:44:58 +0800

    Added chapter 38 Coding Challenge #1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -145,7 +145,7 @@ console.log(yearsUntilRetirement(2017, "Iman Ezra Khayr"));
 const square = x => (x * x);
 const sum = (x, y) => (x + y);
 const divide = (x, y) => (x / y);
-*/
+
 
 // 6 March 2022 11.31 AM
 // 36. Functions calling other functions
@@ -232,9 +232,9 @@ function cookRiceForGuest(guest) { //where the parameter is guest
     const cupsOfRice = guest * 1;
     const cupsOfWater = cupsOfRice + 1.5;
 
-    const result = console.log(`You need to add ${cupsOfRice} cups of rice 
+    const result = console.log(`You need to add ${cupsOfRice} cups of rice
     and ${cupsOfWater} cups of water in the rice cooker.`);
-    //     const result = alert(`You need to add ${cupsOfRice} cups of rice 
+    //     const result = alert(`You need to add ${cupsOfRice} cups of rice
     // and ${cupsOfWater} cups of water in the rice cooker.`);
     return result;
 }
@@ -280,6 +280,28 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearsUntilRetirement(1985, 'Ahmad Khairi'));
+*/
 
+// 7 March 2022 @ 12.36 pm
+// 38. Coding Challenge #1
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated(so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+//     Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+// Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 
 
