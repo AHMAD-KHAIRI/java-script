b9788fe9587c7f6f5459451aa9e0e0fc68e237eb
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-04-05 23:53:48 +0800

    Configured editor and installed live-server

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+// if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2022 - birthYear;
+
+// when just keying cl it will auto-complete console.log()
+console.log(calcAge(1985));
