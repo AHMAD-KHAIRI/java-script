8d01ca53be51288f1d87767b8a98b3b3f33bd647
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-04-23 22:44:18 +0800

    Added Node-RED JSON object for testing

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -8,3 +8,35 @@ const calcAge = birthYear => 2022 - birthYear;
 
 // when just keying cl it will auto-complete console.log()
 console.log(calcAge(1985));
+
+// Node-RED JSON object
+const testSignal =
+    [
+        {
+            "name": "test endpoint",
+            "description": "test",
+            "unit": "",
+            "values":
+                [
+                    {
+                        "value": 2, "timestamp": "2022-04-08T05:32:47.409Z"
+                    }
+                ]
+        },
+        {
+        "name": "test endpoint 1",
+        "description": "test",
+        "unit": "",
+        "values": [{
+            "value": 3, "timestamp": "2022-04-08T05:32:47.409Z"
+        }]
+    }
+];
+
+
+console.log(testSignal);
+console.log(testSignal.length);
+console.log(testSignal[0].values[0].value);
+console.log(testSignal[0].values[0].timestamp);
+console.log(testSignal[1].values[0].value);
+console.log(testSignal[1].values[0].timestamp);
