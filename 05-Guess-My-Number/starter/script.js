de64f84c6266f1a7a1d561ccd98568ccf79d1c70
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-05-21 12:27:07 +0800

    Added how to select an html element using document.querySelector method and also how to select text content of a html element using .textContent property

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// how to select an html element in javascript: document.querySelector('<selectors>')
+// queryselector selects the element of class = "message"
+// in this case .message is referring to class = "message"
+console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent); // Property .textContent selects the text inside the element
