a2c7dc3a8f86894b05fdded8aa7c8c8ea5b4209c
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-05-22 13:22:36 +0800

    added how to select and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,25 @@
 'use strict';
 
 // how to select an html element in javascript: document.querySelector('<selectors>')
-// queryselector selects the element of class = "message"
+// querySelector selects the element of class = "message"
 // in this case .message is referring to class = "message"
+// if the element uses id then we can use #message
 console.log(document.querySelector('.message'));
-console.log(document.querySelector('.message').textContent); // Property .textContent selects the text inside the element
+
+// Property .textContent selects and reads the text inside the element
+console.log(document.querySelector('.message').textContent);
+
+// DOM Manipulation examples:
+// By using the Property .textContent we can also replace/change the text inside the element --> This is DOM manipulation
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent); //outputs Correct Number!
+
+// selects the element of class "number" and "score" and replaces the text inside the element
+document.querySelector('.number').textContent = 13; // '?' is replaced with 13
+document.querySelector('.score').textContent = 10; // 20 is replaced with 10
+
+// to display the value inside the input element with class "guess", we use the Property value
+console.log(document.querySelector('.guess').value);
+
+// we can insert a value inside the element
+document.querySelector('.guess').value = 15;
