937d7fdd8a37f5f27c2ca2abb817082bd717f395
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-09-02 00:15:34 +0800

    first, variables are created that stores the elements according to its class using document.querySelector method including a new method to select all elements using querySelectorAll method and lastly use a for loop to select each of the element in the same class to manipulate each of the element

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// const btnsOpenModal = document.querySelector('.show-modal');
+// using querySelector method has a limitation where if we have multiple elements using the same class, the querySelector method will only select the first element. So in this case to select all elements, we use querySelectorAll method
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+// we can use for loop to select multiple elements in the same class and do something with the element
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}
